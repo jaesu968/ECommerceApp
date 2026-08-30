c4ae670f65df2acce38efda5dbad1ca671055c9a
jaesu968 <mazterk968@gmail.com>
2026-08-30 13:03:27 -0500

    variable clean up for app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var passport = require('passport'); // import passport
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const passport = require('passport'); // import passport
+const logger = require('morgan');
 const dotenv = require('dotenv').config(); // import dotenv
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,6 +32,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', authRouter);
 
 
 // catch 404 and forward to error handler
